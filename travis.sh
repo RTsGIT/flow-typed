669ab283d2128632a9c5b306910069b9d179279f
Jeff Morrison <jeffmo@fb.com>
2016-06-07 09:12:45 -0400

    Only run tests for changed libdefs in travis

--- travis.sh
+++ travis.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-#set -o errexit
+set -o errexit
+set -v
+
+# We manage the git clone manually so that we have "origin/master" available to 
+# us for diffing
+if [ -v TRAVIS ]; then
+  git fetch origin master
+  git clone https://github.com/$TRAVIS_REPO_SLUG.git $TRAVIS_REPO_SLUG
+  cd $TRAVIS_REPO_SLUG
+  git fetch origin
+  git checkout "origin/$TRAVIS_BRANCH"
+fi
 
 cd cli && \
 npm install && \
 ./node_modules/.bin/flow && \
-node dist/cli.js validate-defs && \
-node dist/cli.js run-tests
+node dist/cli.js run-ci-tests ..
